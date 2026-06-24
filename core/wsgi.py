75e15cfccc79c0c3e984c3bbd4094b6d012f16dc
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-06-23 22:14:36 -0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -5,5 +5,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
 
-# Adicione esta linha para o Vercel:
+# ---> ADICIONE ESTAS LINHAS PARA FAZER A VERCEL CRIAR AS TABELAS SOZINHA:
+from django.core.management import call_command
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Erro ao rodar migrações: {e}")
+# <---
+
 app = application
